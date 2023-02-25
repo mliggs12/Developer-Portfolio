ebe99abaa456d6a67f7c068b3b1e6213eb893fb0
Michael <mliggs12@gmail.com>
2023-02-24 22:59:21 -0700

    Updated base.html and app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+
+    return render_template('index.html', page='home')
+
+@app.route('/')
+def projects():
+
+    return render_template('projects.html', page='projects')
+
+@app.route('/')
+def contact():
+
+    return render_template('contact.html', page='contact')

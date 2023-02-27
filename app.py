cce08ebbff105108cef31fd1f3a3751eb0cd1de5
Michael <mliggs12@gmail.com>
2023-02-27 00:39:33 -0700

    Updated navbar

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 from flask import Flask, render_template, url_for
 
-
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
 
-    return render_template('index.html', page='home')
+    return render_template('index.html')
 
 @app.route('/')
 def projects():
 
-    return render_template('projects.html', page='projects')
+    return render_template('projects.html')
 
 @app.route('/')
 def contact():
 
-    return render_template('contact.html', page='contact')
+    return render_template('contact.html')
